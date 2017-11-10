3a74171dc30c29317940410bb6d944124ffc5a8f
Fanghua Wu <fanghuawu@gmail.com>
2017-11-10 14:49:30 +1100

    lecture 01-03

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,19 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+#'matplotlib.sphinxext.mathmpl',
+#'matplotlib.sphinxext.only_directives',
+#'matplotlib.sphinxext.plot_directive',
+#'matplotlib.sphinxext.ipython_directive',
+ 'sphinx.ext.imgmath',
+ 'sphinx.ext.autodoc',
+ 'sphinx.ext.doctest',
+ 'nbsphinx',
+#'ipython_console_highlighting',
+#'inheritance_diagram',
+ 'numpydoc']
+#exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,7 +51,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst','.md','.ipynb','tex']
 
 # The master toctree document.
 master_doc = 'index'
